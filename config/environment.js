91698e00ba2022f7af2c61e1df3b41db4dfa107a
ExpDev07 <marius.richardsen@hiof.no>
2020-02-04 01:47:00 +0100

    pushed prod

--- config/environment.js
+++ config/environment.js
@@ -44,7 +44,8 @@ module.exports = function(environment) {
   }
 
   if (environment === 'production') {
-    // here you can enable a production-specific feature
+    // GitHub pages root url.
+    ENV.rootURL = 'coronavirus-tracker/';
   }
 
   return ENV;
