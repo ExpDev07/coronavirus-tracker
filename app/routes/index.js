6a0d702317f428807a7d85315a9246d779bd8bfd
ExpDev07 <marius.richardsen@hiof.no>
2020-02-05 03:52:59 +0100

    Deploying to gh-pages!

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,4 +1,18 @@
 import Route from '@ember/routing/route';
 
 export default class IndexRoute extends Route {
+
+  async model() {
+    await fetchData();
+  }
+
 }
+
+/**
+ * Fetches the data.
+ */
+async function fetchData() {
+
+}
+
+
