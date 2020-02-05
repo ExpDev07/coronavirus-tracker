26ba60ead0ac314e1ce19820876414e9a53104d2
ExpDev07 <marius.richardsen@hiof.no>
2020-02-05 16:44:56 +0100

    googleapis

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,4 +1,10 @@
 import Route from '@ember/routing/route';
+import { google } from 'googleapis';
+
+/**
+ * Google Sheets instance.
+ */
+const sheets = google.sheets();
 
 export default class IndexRoute extends Route {
 
@@ -12,6 +18,11 @@ export default class IndexRoute extends Route {
  * Fetches the data.
  */
 async function fetchData() {
+
+  // Get the spreadsheet.
+  const doc = await sheets.spreadsheets.get('1wQVypefm946ch4XDp37uZ-wartW4V7ILdg-qYiDXUHM');
+
+  doc.
 
 }
 
